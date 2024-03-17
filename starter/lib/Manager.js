0247affd938aeb5659fff87c2cecec24ca380161
Quarshie <asi37375@hotmail.com>
2024-03-17 20:09:23 +0000

    Manager.js added

--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,1 +1,32 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// Manager.js
+
+class Manager {
+    constructor(name, id, email, officeNumber) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.officeNumber = officeNumber;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+
+    getRole() {
+        return "Manager";
+    }
+}
+
+module.exports = Manager;
