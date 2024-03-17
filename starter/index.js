d4ea36966b03f0f099ce8d7b5d67760d0d9c37b6
Quarshie <asi37375@hotmail.com>
2024-03-17 20:12:39 +0000

    added engineer questions

--- starter/index.js
+++ starter/index.js
@@ -35,3 +35,26 @@ const managerQuestions = [
         message: "Enter the manager's office number:",
     }
 ];
+
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the engineer's name:",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Enter the engineer's employee ID:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter the engineer's email:",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter the engineer's GitHub username:",
+    }
+];
